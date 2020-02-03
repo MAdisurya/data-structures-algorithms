1317c560ac1c1a2401caecb1c171f6d1a8be3e73
Mario Adisurya <>
2020-02-04 10:25:17 +1300

    Initial implementation of Sliding Window algorithm
    + SlidingWindow text explanation
    + Class definition of SlidingWindow

---
+++ algorithms/sliding-window/SlidingWindow.cs
@@ -0,0 +1,22 @@
+﻿/**
+ * Sliding Window C# implementation written by Mario Adisurya
+ * Feel free to fork it from my GitHub - https://github.com/MAdisurya/data-structures-algorithms
+ */
+
+using System;
+
+class SlidingWindow
+{
+    public uint LongestSubstring(string _S)
+    {
+        return 0;
+    }
+}
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
