75555e82129eeb8852112c568a1d762f649da0d9
Mario Adisurya <>
2020-02-28 09:33:22 +1300

    + Initial implementation of MergeSort algorithm

---
+++ algorithms/merge-sort/MergeSort/MergeSort.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class MergeSortAlgorithm<T> where T : IComparable
+{
+    public T[] MergeSort(T[] _array)
+    {
+        return null;
+    }
+
+    public T[] Merge(T[] _left, T[] _right)
+    {
+        return null;
+    }
+}
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
