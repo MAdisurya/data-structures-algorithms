d74a35257d30d0d14999ad3fd9e83f43b71e8d0f
Mario Adisurya <>
2020-03-03 09:16:31 +1300

    + Initial implementation of Depth First Search (DFS) + Added explanation and example of DFS

---
+++ algorithms/depth-first-search/DepthFirstSearch/DepthFirstSearch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Search<T> where T : IComparable
+{
+    public T[] GraphDFS(T at)
+    {
+        return null;
+    }
+}
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
