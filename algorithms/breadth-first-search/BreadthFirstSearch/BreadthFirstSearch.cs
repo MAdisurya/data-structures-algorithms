a3c2290b629a3efa2d917daba708623e26a2dc76
Mario Adisurya <>
2020-03-18 08:39:17 +1300

    * Completed implementation of BFS with path reconstruction

--- algorithms/breadth-first-search/BreadthFirstSearch/BreadthFirstSearch.cs
+++ algorithms/breadth-first-search/BreadthFirstSearch/BreadthFirstSearch.cs
@@ -40,6 +40,43 @@ class Graph
     }
 
     /// <summary>
+    /// Reconstructs shortest path from start vertex to end vertex
+    /// by backtracking from the end vertex and traversing it's parent.
+    /// </summary>
+    /// <param name="s">The start vertex</param>
+    /// <param name="e">The end vertex</param>
+    /// <param name="prev">The array where the parents of each vertex is stored</param>
+    /// <returns>Shortest path from start to end</returns>
+    private List<int> ReconstructPath(int s, int e, int[] prev)
+    {
+        // For storing and returning the reconstructed path
+        List<int> path = new List<int>();
+
+        // We iterate and traverse the prev array starting from the end vertex
+        // The idea is to backtrack from the end vertex by getting it's parent
+        // vertex and storing it into the path array
+        // We continue this process until we've reached a vertex with no parent
+        for (int at = e; at > -1; at = prev[at])
+        {
+            path.Add(at);
+        }
+
+        // Reverse the path so we have s > e, not e > s
+        //ReverseArray(ref path, size);
+        path.Reverse();
+
+        // Handle case where the iteration may not have reached the start vertex
+        // (This is possible if we have a disjoint graph)
+        // Just need to check if the first item is the same as the start vertex
+        if (path[0] == s)
+        {
+            return path;
+        }
+
+        return null;
+    }
+
+    /// <summary>
     /// Adds an edge into the graph
     /// </summary>
     public void AddEdge(int v, int w)
@@ -112,13 +149,24 @@ class Graph
                 }
             }
         }
+
+        Console.WriteLine();
+        Console.WriteLine($"Reconstructed Path from {s} to {e}:");
+
+        foreach (int i in ReconstructPath(s, e, prev))
+        {
+            Console.Write($"{i} ");
+        }
     }
 }
 
 class MainClass
 {
-    public static void Main(string[] args)
+    public static void Main()
     {
+        // Refer to this image for graph we will be using:
+        // https://raw.githubusercontent.com/MAdisurya/data-structures-algorithms/master/algorithms/breadth-first-search/BreadthFirstSearch/bfs-example.JPG
+
         Graph g = new Graph(13);
 
         Console.WriteLine("Breadth First Traversal (BFS)");
@@ -142,10 +190,17 @@ class MainClass
         g.AddEdge(12, 2);
         Console.WriteLine();
 
-        Console.WriteLine("BFS starting from 0");
+        Console.WriteLine("BFS starting from 0:");
 
         g.BFS(0, 4);
 
         Console.WriteLine();
+
+        // Expected Output:
+        // -----------------
+        // BFS starting from 0:
+        // 0 7 9 11 3 6 8 10 2 4 5 1 12
+        // Reconstructed Path from 0 to 4:
+        // 0 7 3 4
     }
 }
