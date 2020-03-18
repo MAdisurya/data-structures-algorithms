84576a5d08bae3cadf17753c1aaff58edd44e385
Mario Adisurya <>
2020-03-19 09:03:40 +1300

    + Added explanation of Trie/Prefix Tree. Still need to add Searching example.

---
+++ data-structures/trie/Trie/Trie.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Trie
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
